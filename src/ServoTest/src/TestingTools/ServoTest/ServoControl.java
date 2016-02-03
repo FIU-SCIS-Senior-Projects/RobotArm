7c604c172a7eedf5379574e26263643ea1f57510
ccox016 <ccox016@fiu.edu>
2016-02-03 10:41:46 -0500

    Added Discovery Lab files and positinsModel.java

---
+++ src/ServoTest/src/TestingTools/ServoTest/ServoControl.java
@@ -0,0 +1,23 @@
+// ServoControl class
+// @author Curtis Cox
+// ServoControl is the contoller class for the Servo test suite
+// for FIU Discovery Lab Telebot - Arms
+
+import RobotArm.src.TestingTools.ServoTest.PossitionsModel;
+
+
+
+public class ServoControl {
+	
+	private setPossition(int value, int servoID)
+	{
+		
+	}
+	
+	public int newValue (String value, int servoID)
+	{
+		int correctedValue = value;
+		
+		return correctedValue;
+	}
+}
