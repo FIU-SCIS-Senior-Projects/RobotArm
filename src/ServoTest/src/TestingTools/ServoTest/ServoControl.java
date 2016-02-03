d95d1531f16a6c60bec8180bf7d01e0ef19d0931
ccox016 <ccox016@fiu.edu>
2016-02-03 11:38:31 -0500

    added jssc.serialPort

--- src/ServoTest/src/TestingTools/ServoTest/ServoControl.java
+++ src/ServoTest/src/TestingTools/ServoTest/ServoControl.java
@@ -1,23 +1,41 @@
+package TestingTools.ServoTest;
 // ServoControl class
 // @author Curtis Cox
 // ServoControl is the contoller class for the Servo test suite
 // for FIU Discovery Lab Telebot - Arms
 
-import RobotArm.src.TestingTools.ServoTest.PossitionsModel;
+import TestingTools.ServoTest.PositionsModel;
+import ThirdParty.jssc.SerialPort;
 
 
 
 public class ServoControl {
 	
-	private setPossition(int value, int servoID)
+	private static ServoControl singleton = null;
+	private PositionsModel servoModel = null;
+	
+	//Private constructor to force use of getSingleton()
+	private ServoControl()
+	{
+		servoModel = PositionsModel.getSingleton();
+	}
+	
+	public static ServoControl getSingleton(){
+		if(singleton == null)
+			singleton = new ServoControl();
+		return singleton;
+	}
+	
+	private void setPosition(int value, int servoID)
 	{
 		
 	}
 	
 	public int newValue (String value, int servoID)
 	{
-		int correctedValue = value;
-		
+		int correctedValue = Integer.parseInt(value);
+		correctedValue = servoModel.setSevoValue(correctedValue, servoID);
+		setPosition(correctedValue, servoID);
 		return correctedValue;
 	}
 }
