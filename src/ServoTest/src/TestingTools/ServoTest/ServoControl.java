fe7507c318d64ff7903eb4f683ab348460834805
ccox016 <ccox016@fiu.edu>
2016-04-01 16:37:19 -0400

    corrected port number

--- src/ServoTest/src/TestingTools/ServoTest/ServoControl.java
+++ src/ServoTest/src/TestingTools/ServoTest/ServoControl.java
@@ -38,7 +38,7 @@ public class ServoControl implements SerialPortEventListener {
 		feedBack = ServoFeedback.getSingleton();
 		
 		//Serial Port init
-		serialPortName = "Com8"; //"/dev/TelebotArms";
+		serialPortName = "/dev/TelebotArms";
 		baudRate = 57600;
 		dataBits = SerialPort.DATABITS_8;
 		stopBits = SerialPort.STOPBITS_1;
